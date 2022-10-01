f2a9028dcf472c06c47408bd24c8e3c5fd4e61e2
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2022-10-01 17:55:57 +0300

    Update and rename grading_students to grading_students.py

---
+++ grading_students.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+for a0 in range(n):
+    x = int(input().strip())
+    
+    if x >= 38:
+        if x % 5 > 2:
+            while x % 5 != 0: x += 1
+    print(x)
