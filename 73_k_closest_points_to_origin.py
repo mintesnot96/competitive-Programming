b0ace72108aed75e6b65d2f86d717286534cf997
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2022-10-01 22:47:24 +0300

    Update 73_k_closest_points_to_origin.py

--- 73_k_closest_points_to_origin.py
+++ 73_k_closest_points_to_origin.py
@@ -1,24 +1,9 @@
-from cmath import sqrt
-from locale import currency
-
 class Solution(object):
-    def kClosest(selef,points, k):
-        closest = []
-        for i in range(k):
-            close_distance = pow((pow(points[0][0],2)+pow(points[0][1],2)), 0.5)
-            curr_closest = [] 
-         
-            for k,j in points:
-                distance = pow((pow(k,2)+pow(j,2)), 0.5)
-            
-                if distance <= close_distance:
-                    close_distance = distance
-                    curr_closest = []
-                    curr_closest.append(k)
-                    curr_closest.append(j)
-                    
-                else:
-                    continue
-            closest.append(curr_closest)
-            points.pop(points.index(curr_closest))
-        return closest
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        points.sort(key = lambda x: x[0]**2 + x[1]**2)
+        return points[:k]
