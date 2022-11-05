50aaf9487709833532be04b415436a4827ea3e95
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2022-11-05 22:27:23 +0300

    Create 227. Basic Calculator II leetcode.py
    # question
    # https://leetcode.com/problems/basic-calculator-ii/

---
+++ 227. Basic Calculator II leetcode.py
@@ -0,0 +1,43 @@
+# question
+# https://leetcode.com/problems/basic-calculator-ii/
+class Solution(object):
+    def calculate(self, s):
+        num_stack = []
+        num = 0
+        op = '+'
+        for s_s in s:
+            if s_s == ' ':
+                continue
+            elif s_s.isdigit():
+                num = num * 10 + int(s_s)
+            else:
+                if op == '*':
+                    x = num_stack.pop()
+                    num_stack.append(x * num)
+                elif op == '/':
+                    x = num_stack.pop()
+                    if x < 0:
+                        num_stack.append(-((-x) / num))
+                    else:
+                        num_stack.append(x / num)
+                elif op == '-':
+                    num_stack.append(-1 * num)
+                else:
+                    num_stack.append(num)
+                op = s_s
+                num = 0
+        else:
+            if op == '*':
+                x = num_stack.pop()
+                num_stack.append(x * num)
+            elif op == '/':
+                x = num_stack.pop()
+                if x < 0:
+                    num_stack.append(-((-x)/ num))
+                else:
+                    num_stack.append(x / num)
+            elif op == '-':
+                num_stack.append(-1 * num)
+            else:
+                num_stack.append(num)
+        return sum(num_stack)
